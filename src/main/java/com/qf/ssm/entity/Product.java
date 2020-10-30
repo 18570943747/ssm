e818669ded48675f3e86fdf229049db9aa9e4d59
liuyue <657778523@qq.com>
2020-10-30 20:46:32 +0800

    ssm

---
+++ src/main/java/com/qf/ssm/entity/Product.java
@@ -0,0 +1,24 @@
+package com.qf.ssm.entity;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    /**
+     *
+     */
+    private Integer productId;
+
+    /**
+     *
+     */
+    private String name;
+
+    /**
+     *
+     */
+    private Integer status;
+
+
+}
+
